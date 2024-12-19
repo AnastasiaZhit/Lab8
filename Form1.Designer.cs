b336efa70492a960e752c8ac81e217a5a470985e
Anastasiya231102@yandex.ru <Anastasiya231102@yandex.ru>
2024-12-19 23:18:56 +0700

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Lab8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(189, 351);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 16);
+            this.label1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(152, 52);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(486, 232);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(46, 52);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 51);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Нулевая матрица";
+            this.button1.UseVisualStyleBackColor = true;
+            button1.Click += (s, e) => GenerateMatrix(new ZeroMatrixGenerator());
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(46, 109);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 42);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Еденичная матрица";
+            this.button2.UseVisualStyleBackColor = true;
+            button2.Click += (s, e) => GenerateMatrix(new DiagonalOnesGenerator());
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(46, 157);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 83);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Главная диагональ рандомные значения";
+            this.button3.UseVisualStyleBackColor = true;
+            button3.Click += (s, e) => GenerateMatrix(new RandomDiagonalGenerator());
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(46, 246);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 99);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Заполнение всей матрицы рандомными значениями";
+            this.button4.UseVisualStyleBackColor = true;
+            button4.Click += (s, e) => GenerateMatrix(new RandomMatrixGenerator());
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(46, 351);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 73);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Заполнение всей матрицы змейкой";
+            this.button5.UseVisualStyleBackColor = true;
+            button5.Click += (s,e) => GenerateMatrix(new SnakeMatrixGenerator());
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(688, 452);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab8
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void GenerateMatrix(MatrixGenerator generator)
+        {
+            // Генерация матрицы
+            int[,] matrix = generator.GenerateMatrix(3, 3); // 3x3 матрица.
+
+            // Вывод матрицы в DataGridView
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+            for (int i = 0; i < matrix.GetLength(1); i++)
+            {
+                dataGridView1.Columns.Add($"Col{i}", $"Колонка {i + 1}");
+            }
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                dataGridView1.Rows.Add();
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    dataGridView1.Rows[i].Cells[j].Value = matrix[i, j];
+                }
+            }
+
+            // Вычисление характеристик
+            int diagonalSum = generator.CalculateDiagonalSum(matrix);
+            int totalSum = generator.CalculateTotalSum(matrix);
+            int maxValue = generator.GetMaxValue(matrix);
+            int minValue = generator.GetMinValue(matrix);
+
+            // Вывод характеристик на экран
+            label1.Text = $"Сумма диагонали: {diagonalSum}, Сумма всех элементов: {totalSum}\n" +
+                               $"Макс: {maxValue}, Мин: {minValue}";
+        }
+
+        private void GenerateMatrix(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    // Шаблонный метод
+
+    public abstract class MatrixGenerator
+    {
+        public abstract int[,] GenerateMatrix(int rows, int cols);
+
+        public int CalculateDiagonalSum(int[,] matrix)
+        {
+            int sum = 0;
+            int size = Math.Min(matrix.GetLength(0), matrix.GetLength(1));
+            for (int i = 0; i < size; i++)
+            {
+                sum += matrix[i, i];
+            }
+            return sum;
+        }
+
+        public int CalculateTotalSum(int[,] matrix)
+        {
+            return matrix.Cast<int>().Sum();
+        }
+
+        public int GetMaxValue(int[,] matrix)
+        {
+            return matrix.Cast<int>().Max();
+        }
+
+        public int GetMinValue(int[,] matrix)
+        {
+            return matrix.Cast<int>().Min();
+        }
+    }
+
+    // Генерация единичной матрицы по диагонали
+    public class DiagonalOnesGenerator : MatrixGenerator
+    {
+        public override int[,] GenerateMatrix(int rows, int cols)
+        {
+            int[,] matrix = new int[rows, cols];
+            for (int i = 0; i < Math.Min(rows, cols); i++)
+            {
+                matrix[i, i] = 1;
+            }
+            return matrix;
+        }
+    }
+
+    // Генерация нулевой матрицы
+    public class ZeroMatrixGenerator : MatrixGenerator
+    {
+        public override int[,] GenerateMatrix(int rows, int cols)
+        {
+            return new int[rows, cols]; // Все элементы автоматически инициализируются 0.
+        }
+    }
+
+    // Генерация случайной матрицы
+    public class RandomMatrixGenerator : MatrixGenerator
+    {
+        public override int[,] GenerateMatrix(int rows, int cols)
+        {
+            int[,] matrix = new int[rows, cols];
+            Random random = new Random();
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    matrix[i, j] = random.Next(0, 100); // Случайные числа от 0 до 99.
+                }
+            }
+            return matrix;
+        }
+    }
+
+    // Генерация случайной матрицы по диагонали
+    public class RandomDiagonalGenerator : MatrixGenerator
+    {
+        public override int[,] GenerateMatrix(int rows, int cols)
+        {
+            int[,] matrix = new int[rows, cols];
+            Random random = new Random();
+            for (int i = 0; i < Math.Min(rows, cols); i++)
+            {
+                matrix[i, i] = random.Next(0, 100);
+            }
+            return matrix;
+        }
+    }
+    // Генерация матрицы по спирали
+    public class SnakeMatrixGenerator : MatrixGenerator
+    {
+        public override int[,] GenerateMatrix(int rows, int cols)
+        {
+            int[,] matrix = new int[rows, cols]; // Матрица, которую будем заполнять
+            for (int i = 0; i < cols; i++)
+            {
+                for (int j = 0; j < rows; j++)
+                {
+                    matrix[i, j] = i % 2 == 1 ? i * cols + j + 1 : rows * (i + 1) - j;
+                }
+            }
+
+            return matrix;
+        }
+    }
+}
